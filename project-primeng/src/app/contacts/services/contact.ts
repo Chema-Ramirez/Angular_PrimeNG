edc95ea2c4dadc9af55245050deae3c8d3fc0cf2
Jose Ramirez <joseramqui93@gmail.com>
2025-07-15 13:50:27 +0200

    set up service app

--- project-primeng/src/app/contacts/services/contact.ts
+++ project-primeng/src/app/contacts/services/contact.ts
@@ -1,8 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Contact {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Contact {
-  
+  private API = 'http://localhost:3000/contacts';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(this.API);
+  }
+
+  create(contact: Omit<Contact, 'id'>): Observable<Contact> {
+    return this.http.post<Contact>(this.API, contact);
+  }
+
+  update(contact: Contact): Observable<Contact> {
+    return this.http.put<Contact>(`${this.API}/${contact.id}`, contact);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API}/${id}`);
+  }
 }
