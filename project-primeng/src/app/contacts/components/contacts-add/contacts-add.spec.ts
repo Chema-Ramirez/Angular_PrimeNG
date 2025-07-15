8fc12deb336af018ad7a7f18708166425b128986
Jose Ramirez <joseramqui93@gmail.com>
2025-07-15 13:10:40 +0200

    project started

---
+++ project-primeng/src/app/contacts/components/contacts-add/contacts-add.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContactsAdd } from './contacts-add';
+
+describe('ContactsAdd', () => {
+  let component: ContactsAdd;
+  let fixture: ComponentFixture<ContactsAdd>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ContactsAdd]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ContactsAdd);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ project-primeng/src/app/contacts/components/contacts-add/contacts-add.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-add',
+  imports: [],
+  templateUrl: './contacts-add.html',
+  styleUrl: './contacts-add.css'
+})
+export class ContactsAdd {
+
+}
